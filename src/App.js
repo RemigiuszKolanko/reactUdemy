8c48bc79c3c8fed78ece0a978cafe8901aefa5ca
Remigiusz Kolanko <remigiusz.kolanko@gmail.com>
2019-07-10 21:29:34 +0200

    Basic of components implemented

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi! I am a react app.</h1>
+      <p>It is real working!</p>
+      <Person name="Remigiusz" age="30" />
+      <Person name="Ewa" age="29"/>
+      <Person name="Zuzia" age="1"/>
     </div>
   );
 }
