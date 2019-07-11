f394e06e6acbec7739b2a1fa12aa41643c7c42bb
Remigiusz Kolanko <remigiusz.kolanko@gmail.com>
2019-07-11 21:01:21 +0200

    data passed between two components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi! I am a react app.</h1>
-      <p>It is real working!</p>
-      <Person name="Remigiusz" age="30" />
-      <Person name="Ewa" age="29"/>
-      <Person name="Zuzia" age="1"/>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      { name: "Remigiusz", age: 28 },
+      { name: "Ewa", age: 27 },
+      { name: "Zuzia", age: 1 },
+      { name: "Oliwier", age: 9 }
+    ],
+    otherState: "Other state"
+  }
+
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        { name: "Remi", age: 28 },
+        { name: "Ewe", age: 27 },
+        { name: "Zuz", age: 1 },
+        { name: "Oliwier", age: 9 }
+      ]
+    })
+  }
+
+  render() {
+    return(
+      <div className = "App" >
+        <h1>Hi! I am a react app.</h1>
+        <p>It is real working!</p>
+        <button onClick={this.switchNameHandler}>Switch name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My hobbies: squash.</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <Person name={this.state.persons[3].name} age={this.state.persons[3].age} />
+      </div>
+    );
+  }
+  
 }
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const person = (props) => {
-    return <p>I'm a {props.name} and I am {props.age} years old!</p>
-};
+class Person extends Component {
+    render() {
+        return <p>Hi! My name is {this.props.name}. I am {this.props.age} years old. {this.props.children}</p>
+    }
+}
 
-
-
-export default person;
+export default Person;
